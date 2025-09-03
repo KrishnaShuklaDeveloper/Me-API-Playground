0a94bc7a0ea3aaf2486946f617ed16f515c6058d
Krishna Shukla <krishnashukla9219448687@gmail.com>
2025-09-04 01:06:16 +0530

    Update script.js

--- api/static/api/script.js
+++ api/static/api/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "/api";
 
 let allProjects = [];
 let allSkills = [];
@@ -125,3 +125,4 @@ window.onload = () => {
   loadProfile();
   loadData();
 };
+
