34ca45a3f31ad204e74612ef54501e0483b04dc2
Krishna Shukla <krishnashukla9219448687@gmail.com>
2025-09-04 11:52:07 +0530

    Update seed_me.py

--- seed_me.py
+++ seed_me.py
@@ -15,12 +15,12 @@ Skill.objects.all().delete()
 profile = Profile.objects.create(
     name="Krishna Shukla",
     email="krishnashukla9219448687@gmail.com",
-    education=(
-        "Bachelor of Technology (B.Tech) in Computer Science & Engineering "
-        "R.R. Institute of Modern Technology, Lucknow | 2025 | CGPA: 7.9/10\n"
-        "Higher Secondary (12th Grade) – Bal Nikunj Inter College, 2021 (86%)\n"
-        "Secondary (10th Grade) – Bal Nikunj Inter College, 2019 (81%)"
-    ),
+    education = (
+    "Bachelor of Technology (B.Tech) in Computer Science & Engineering - "
+    "R.R. Institute of Modern Technology, Lucknow | 2025 | CGPA: 7.9/10\n"
+    "Higher Secondary (12th Grade) – Bal Nikunj Inter College, 2021 (86%)\n"
+    "Secondary (10th Grade) – Bal Nikunj Inter College, 2019 (81%)"
+)
     summary=(
         "I’m a Full-Stack Developer with experience building real-world projects like a Voting Management System, Library Management System. I work with technologies such as Python & Django, PHP (Laravel), JavaScript, React, and SQL databases, and enjoy creating applications that are both efficient and user-friendly. From designing responsive UIs to building secure APIs and handling database logic, I take ownership of the full development cycle. I’ve also integrated features like live charts, authentication, dark/light themes, and PDF/Excel exports in my projects. I enjoy problem-solving, experimenting with new tools, and continuously improving my skills to deliver impactful solutions."
     ),
@@ -42,7 +42,7 @@ project1 = Project.objects.create(
     profile=profile,
     title="Digital Library Management System",
     description="Library management system using Django",
-    links={"GitHub": "https://github.com/KrishnaShuklaDeveloper/Digital-Library-Management-System"},
+    links={"gitHub": "https://github.com/KrishnaShuklaDeveloper/Digital-Library-Management-System"},
 )
 project1.skills.add(python, django, html)
 
@@ -50,7 +50,7 @@ project2 = Project.objects.create(
     profile=profile,
     title="Admin-Controlled Voting System",
     description="Secure voting system built with Django",
-    links={"GitHub": "https://github.com/KrishnaShuklaDeveloper/Voting-System"},
+    links={"gitHub": "https://github.com/KrishnaShuklaDeveloper/Admin-Controlled-Voting-System"},
 )
 project2.skills.add(django, javascript, html)
 
@@ -58,9 +58,10 @@ project3 = Project.objects.create(
     profile=profile,
     title="FitLife and Wellness Information Portal",
     description="Health & fitness portal with full-stack features",
-    links={"GitHub": "https://github.com/KrishnaShuklaDeveloper/FitLife-Portal"},
+    links={"gitHub": "https://github.com/KrishnaShuklaDeveloper/FitLife-and-Wellness-Information-Portal/tree/main/Health%20and%20fitness"},
 )
 project3.skills.add(python, django, javascript, css)
 
 print("✅ Seeding complete: Profile, Projects, Skills added!")
+
 
